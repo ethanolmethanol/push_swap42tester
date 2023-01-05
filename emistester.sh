a0c1bcf68d79551c1f7f7a49492a03da0e7399a1
Ethan MIS <emis@e1r8p14.clusters.42paris.fr>
2023-01-05 18:03:39 +0100

    Added colour and the possibility of choosing a checker.

--- emistester.sh
+++ emistester.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 perm() {
 	items="$1"
 	out="$2"
@@ -13,8 +18,8 @@ fact() {
 	fact=1
 	while [ $num -gt 1 ]
 	do
-	fact=$((fact * num))  #fact = fact * num
-	num=$((num - 1))      #num = num - 1
+	fact=$((fact * num))
+	num=$((num - 1))
 	done
 	echo "$fact" && return
 }
@@ -25,19 +30,19 @@ checkallperms() {
 	do
 		a=$(echo $n | sed 's/./& /g')
 		b=$(echo ./push_swap $a | tr '\0' ' ')
-		c=$(./push_swap $a | ./checker_linux $a)
+		c=$(./push_swap $a | $4 $a)
 		o=$($b |wc -l)
 		if [ $o -gt $2 ] || [ "$c" != "OK" ]; then
-			echo "Failed for $n: $o operations (max is $2), checker is $c"
+			echo -e "${RED}Failed for $n: $o operations (max is $2), checker is $c${NC}"
 			((s=s+1))
 		else
 			:
 		fi
 	done
 	if [ $s -eq 0 ]; then
-		echo "Success for all $(fact $1) permutations of $1 :)"
+		echo -e "${GREEN}Success for all $(fact $1) permutations of $1 :)${NC}"
 	else
-		echo "Failed $s out of $(fact $1) permutations of $1 :("
+		echo -e "${RED}Failed $s out of $(fact $1) permutations of $1 :(${NC}"
 	fi
 }
 
@@ -63,17 +68,15 @@ checkrange() {
 		clear
 		echo "$(( $m / ($n + 1) )) mean nb of operations"
 		echo "$h highest nb of operations"
-		echo "$(( 100 * $s / ($n + 1) )) % success rate ($s/$(( $n + 1 )))"
+		if [ $s -eq $n ]; then printf "${GREEN}"; else printf "${RED}"; fi
+		echo -e "$(( 100 * $s / ($n + 1) )) % ${NC}success rate ($s/$(( $n + 1 )))"
 	done
 	if [ $s -eq $3 ]; then
-		echo "Success for all $3 tests :)"
+		echo -e "${GREEN}Success for all $3 tests :)${NC}"
 	else
-		echo "Failed $(expr $3 - $s)/$3 tests :("
+		echo -e "${RED}Failed $(expr $3 - $s)/$3 tests :(${NC}"
 	fi
 }
-# while read line ; do perm $line ; done < File
-# echo $(perm 123) | tr ' ' '\n' | tr '' ' '
-#./push_swap $(./randomizer.sh 0 10)
 
 echo "Hello !"
 
@@ -84,15 +87,19 @@ if [ $# -eq 0 ]; then
 	exit 0
 elif [ $# -eq 3 ]; then
 	rm -f log.txt
-	checkrange $1 $2 $3
+	checkrange $1 $2 $3 ./checker_linux
+	echo "More details in log.txt file !"
+	exit 0
+elif [ $# -eq 4 ]; then
+	rm -f log.txt
+	checkrange $1 $2 $3 $4
 	echo "More details in log.txt file !"
 	exit 0
 else
 	echo "Invalid arguments." 
-	echo "Usage : ./emistester.sh <a> <b> <c>"
+	echo "Usage : ./emistester.sh <a> <b> <c> (optional: custom checker)"
 	echo "Where a-b is the range of numbers to shuffle and input in push_swap"
-	echo "And c is the number of times the test will be executed"
+	echo "And c is the number of tests to perform"
+	echo "Example : ./emistester.sh 1 100 100 checker"
 	exit 1
 fi
-
-
